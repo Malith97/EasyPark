33a2156adb31792f5fa9dfa737c3bb81d19b9314
Malith <40682828+Malith97@users.noreply.github.com>
2020-03-05 10:00:24 +0530

    V1.2

--- app/src/main/java/com/synnlabz/easypark/AdminAddBike.java
+++ app/src/main/java/com/synnlabz/easypark/AdminAddBike.java
@@ -1,17 +1,80 @@
 package com.synnlabz.easypark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdminAddBike extends AppCompatActivity {
+
+    private Button mAdd;
+    private EditText mName, mPrice , mExtra;
+
+    private String bikeID, bikeName, bikePrice, bikeExtra ;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mBikeDatabase;
+
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_bike);
+
+        mAdd = (Button) findViewById(R.id.addBicycle);
+
+        mName = (EditText) findViewById(R.id.bikeName);
+        mPrice = (EditText) findViewById(R.id.price);
+        mExtra = (EditText) findViewById(R.id.extra);
+
+        mBikeDatabase = FirebaseDatabase.getInstance().getReference().child("Bikes");
+
+        mAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveBikeInformation();
+            }
+        });
+    }
+
+    private void saveBikeInformation() {
+        bikeName = mName.getText().toString();
+        bikePrice = mPrice.getText().toString();
+        bikeExtra = mExtra.getText().toString();
+
+        DatabaseReference newMessageDb = mBikeDatabase.push();
+
+        final Map bikeInfo = new HashMap();
+
+        bikeInfo.put("Name", bikeName);
+        bikeInfo.put("Price", bikePrice);
+        bikeInfo.put("Extra", bikeExtra);
+
+        newMessageDb.setValue(bikeInfo);
     }
 
     public void goToOrders(View view) {
